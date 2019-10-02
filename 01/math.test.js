a4c3f421d6b2321e32ebf4b8afb474e0c1a23d2e
shanejix <shanejix@vip.163.com>
2019-10-02 16:13:31 +0800

    jest

---
+++ 01/math.test.js
@@ -0,0 +1,10 @@
+const math = require('./math');
+const { sum, minus } = math;
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('minus 2 - 2 to equal 0', () => {
+    expect(minus(2, 2)).toBe(0)
+})
