7cc2ad3f4a41af6c7fb4e4699a9d08ff29016968
shanejix <shanejix@vip.163.com>
2019-10-05 11:35:16 +0800

    snapshot

---
+++ 06_snapshot/snapshot.test.js
@@ -0,0 +1,9 @@
+// Snapshot Testing
+
+
+
+// Snapshot tests are a very useful tool whenever you want to make sure your UI does not change unexpectedly.
+
+
+
+// https://jestjs.io/docs/en/snapshot-testing
